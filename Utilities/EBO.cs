6cc383460c1bf91b0481aabd43b91ffbf30358c5
Nicolas <nicoalbo90@gmail.com>
2013-06-23 23:28:34 -0300

    Partial Obj Broken

---
+++ Utilities/EBO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace Utilities
+{
+    public class EBO
+    {
+        public int firstIndex { get; set; }
+        public int lastIndex { get; set; }
+        public int[] indices { get; set; }
+        public int id { get; set; }
+        public Material material { get; set; }
+        public Matrix4 transformation { get; set; }
+        public string Name { get; set; }
+
+    }
+}
